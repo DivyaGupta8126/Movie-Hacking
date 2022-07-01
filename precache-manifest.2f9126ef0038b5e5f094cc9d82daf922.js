49132eda232105884f743fc56408896d306d3fdd
Divya Gupta <divyagupta8126@gmail.com>
2022-07-02 01:48:33 +0530

    Updates

---
+++ precache-manifest.2f9126ef0038b5e5f094cc9d82daf922.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b79423bebaa074b5a816",
+    "url": "/Movie-Hacking/static/css/main.7eba7657.chunk.css"
+  },
+  {
+    "revision": "b79423bebaa074b5a816",
+    "url": "/Movie-Hacking/static/js/main.b79423be.chunk.js"
+  },
+  {
+    "revision": "b73b78321f36c315e081",
+    "url": "/Movie-Hacking/static/js/1.b73b7832.chunk.js"
+  },
+  {
+    "revision": "975cec607ece063e327a",
+    "url": "/Movie-Hacking/static/js/runtime~main.975cec60.js"
+  },
+  {
+    "revision": "ecabaead9bbf10b9bfd0431e7157eb07",
+    "url": "/Movie-Hacking/static/media/sad.ecabaead.png"
+  },
+  {
+    "revision": "a3ba73f142cab7b83ccde79751e5c578",
+    "url": "/Movie-Hacking/static/media/bg1.a3ba73f1.jpg"
+  },
+  {
+    "revision": "77341efbed68682bb2448fa7d059e4e8",
+    "url": "/Movie-Hacking/index.html"
+  }
+];
